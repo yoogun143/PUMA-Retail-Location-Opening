5ad47d9e68fb36f696fcc5e7c77afaac19478af4
Thanh Hoang <40580775+yoogun143@users.noreply.github.com>
2019-04-21 23:43:29 -0400

    Update bokeh_app.py

--- bokeh_app.py
+++ bokeh_app.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-%matplotlib inline
 import warnings
 from bokeh.models import ColumnDataSource
 warnings.filterwarnings('ignore')
