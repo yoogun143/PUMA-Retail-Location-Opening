8d1babde93bf6214aab3becde11d65111d20a5d5
Thanh Hoang <40580775+yoogun143@users.noreply.github.com>
2019-04-22 00:04:53 -0400

    Update bokeh_app.py

--- bokeh_app.py
+++ bokeh_app.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from bokeh.models import ColumnDataSource
 from sklearn.preprocessing import MinMaxScaler
 from uszipcode import Zipcode
